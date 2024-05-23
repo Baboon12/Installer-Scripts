9d700f50cea4ebbc3c4ed994ea8e7d617326b764
Bhavya <bhavyasura12@gmail.com>
2024-05-23 16:59:56 +0530

    Updated Main

--- main.sh
+++ main.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-scripts=("beef.sh" "metagoofil.sh" "theHarvester.sh" "whatweb.sh" "hydra.sh" "gobuster.sh" "reconng.sh" "nmap.sh" "nikto.sh" "dnsrecon.sh" "dnsenum.sh" "dirsearch.sh" "wfuzz.sh" "websocket-client.sh")
+scripts=("beef.sh" "metagoofil.sh" "theHarvester.sh" "whatweb.sh" "hydra.sh" "gobuster.sh" "reconng.sh" "nmap.sh" "nikto.sh" "dnsrecon.sh" "dnsenum.sh" "dirsearch.sh" "wfuzz.sh" "websocket-client.sh" "httprint.sh" "unicornscan.sh" "curl.sh")
 
-echo "What tool do you want to install?"
-select choice in "beef" "metagoofil" "theHarvester" "whatWeb" "hydra" "gobuster" "reconng" "nmap" "nikto" "dnsrecon" "dnsenum" "dirsearch" "wfuzz" "websocket-client" "exit"; do
-  case "$choice" in
-    "exit")
-      echo "Exiting..."
-      exit 0
-      ;;
-    *)
-      script=${scripts[$REPLY-1]}
-      if [ -x "$script" ]; then
-        echo "Running $script..."
-       ./"$script"
-      else
-        echo "Error: Script $script not found or not executable."
-        exit 1
-      fi
-      ;;
-  esac
+while true; do
+  echo "What tool do you want to install?"
+  select choice in "beef" "metagoofil" "theHarvester" "whatWeb" "hydra" "gobuster" "reconng" "nmap" "nikto" "dnsrecon" "dnsenum" "dirsearch" "wfuzz" "websocket-client" "httprint" "unicornscan" "curl" "exit"; do
+    case "$choice" in
+      "exit")
+        echo "Exiting..."
+        exit 0
+        ;;
+      *)
+        script=${scripts[$REPLY-1]}
+        if [ -x "./scripts/$script" ]; then
+          echo "Running $script..."
+          ./scripts/"$script"
+        else
+          echo "Error: Script $script not found or not executable."
+          exit 1
+        fi
+        break
+        ;;
+    esac
+  done
 done
