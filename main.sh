4e250a93893db16a3df9ac543d2575f2695c1030
viv50578 <84714295+viv50578@users.noreply.github.com>
2024-05-24 19:16:01 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,11 +4,24 @@ scripts=("beef.sh" "metagoofil.sh" "theHarvester.sh" "whatweb.sh" "hydra.sh" "go
 
 while true; do
   echo "What tool do you want to install?"
-  select choice in "beef" "metagoofil" "theHarvester" "whatWeb" "hydra" "gobuster" "reconng" "nmap" "nikto" "dnsrecon" "dnsenum" "dirsearch" "wfuzz" "websocket-client" "httprint" "unicornscan" "curl" "exit"; do
+  select choice in "beef" "metagoofil" "theHarvester" "whatWeb" "hydra" "gobuster" "reconng" "nmap" "nikto" "dnsrecon" "dnsenum" "dirsearch" "wfuzz" "websocket-client" "httprint" "unicornscan" "curl" "install_all" "exit"; do
     case "$choice" in
       "exit")
         echo "Exiting..."
         exit 0
+        ;;
+      "install_all")
+        echo "Installing all tools..."
+        for script in "${scripts[@]}"; do
+          if [ -x "./scripts/$script" ]; then
+            echo "Running $script..."
+            ./scripts/"$script"
+          else
+            echo "Error: Script $script not found or not executable."
+            exit 1
+          fi
+        done
+        break
         ;;
       *)
         script=${scripts[$REPLY-1]}
